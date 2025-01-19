d0af64c67871c39d9a66a1ed1f7e0a5e10009822
Jayyy-J <jota71663@gmail.com>
2025-01-19 12:43:26 -0500

    Se agrego la funcion editValor()

--- module/editElement.py
+++ module/editElement.py
@@ -146,6 +146,57 @@ def editGenesis():
         pressEnter()
 
 
+def editValor():
+    books = abrirArchivo(RUTA_BOOK)
+    musics = abrirArchivo(RUTA_MUSIC)
+    movies = abrirArchivo(RUTA_MOVIES)
+    colections = abrirArchivo(RUTA_COLECCION)
+    titulo = input('Escriba el titulo del elemento que desea cambiar :').capitalize()
+    encontrado = False
+    for book in books:
+        if titulo == book["titulo"]:
+            encontrado = True
+            newValorn = getInt('Escriba la nueva puntuacion nuevo :').capitalize()
+            newValor = str(newValorn)
+            book["valoracion"] = newValor
+            for colection in colections["libros"]:
+                if colection["titulo"] == titulo:
+                    colection["valoracion"] = newValor
+            guardarArchivo(RUTA_COLECCION, colections)
+            guardarArchivo(RUTA_BOOK, books)
+            pressEnter()
+            break
+    for music in musics:
+        if titulo == music["titulo"]:
+            encontrado = True
+            newValorn = getInt('Escriba la nueva puntuacion nuevo :').capitalize()
+            newValor = str(newValorn)
+            music["valoracion"] = newValor
+            for colection in colections["musica"]:
+                if colection["titulo"] == titulo:
+                    colection["valoracion"] = newValor
+            guardarArchivo(RUTA_COLECCION, colections)
+            guardarArchivo(RUTA_MUSIC, musics)
+            pressEnter()
+            break
+
+    for movie in movies:
+        if titulo == movie["titulo"]:
+            encontrado = True
+            newValorn = getInt('Escriba la nueva puntuacion nuevo :').capitalize()
+            newValor = str(newValorn)
+            movie["valoracion"] = newValor
+            for colection in colections["peliculas"]:
+                if colection["titulo"] == titulo:
+                    colection["valoracion"] = newValor
+            guardarArchivo(RUTA_COLECCION, colections)
+            guardarArchivo(RUTA_MOVIES, movies)
+            pressEnter()
+            break
+    if not encontrado:
+        print('No se encontro nada, registralo.')
+        pressEnter()
+
 def editElement():
     while True:
         print(editElementDesign)
@@ -158,8 +209,7 @@ def editElement():
             case 3:
                 editGenesis()
             case 4:
-                #editValor()
-                pass
+                editValor()
             case 5:
                 #editCategory()
                 pass
