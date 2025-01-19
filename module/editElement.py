e1ce91f38f926a1a427502902ff39230d6244fe5
Jayyy-J <jota71663@gmail.com>
2025-01-19 12:33:05 -0500

    Se agrego la funcion editGenesis() es funcional

--- module/editElement.py
+++ module/editElement.py
@@ -96,6 +96,56 @@ def editAuthor():
         print('No se encontro nada, registralo.')
         pressEnter()
 
+
+
+def editGenesis():
+    books = abrirArchivo(RUTA_BOOK)
+    musics = abrirArchivo(RUTA_MUSIC)
+    movies = abrirArchivo(RUTA_MOVIES)
+    colections = abrirArchivo(RUTA_COLECCION)
+    genero = input('Escriba el Genero del elemento que desea cambiar :').capitalize()
+    encontrado = False
+    for book in books:
+        if genero == book["genero"]:
+            encontrado = True
+            newGenero = input('Escriba el genero nuevo :').capitalize()
+            book["genero"] = newGenero
+            for colection in colections["libros"]:
+                if colection["genero"] == genero:
+                    colection["genero"] = newGenero
+            guardarArchivo(RUTA_COLECCION, colections)
+            guardarArchivo(RUTA_BOOK, books)
+            pressEnter()
+            break
+    for music in musics:
+        if genero == music["genero"]:
+            encontrado = True
+            newGenero = input('Escriba el genero nuevo :').capitalize()
+            music["genero"] = newGenero
+            for colection in colections["musica"]:
+                if colection["genero"] == genero:
+                    colection["genero"] = newGenero
+            guardarArchivo(RUTA_COLECCION, colections)
+            guardarArchivo(RUTA_MUSIC, musics)
+            pressEnter()
+            break
+    for movie in movies:
+        if genero == music["genero"]:
+            encontrado = True
+            newGenero = input('Escriba el genero nuevo :').capitalize()
+            movie["genero"] = newGenero
+            for colection in colections["peliculas"]:
+                if colection["genero"] == genero:
+                    colection["genero"] = newGenero
+            guardarArchivo(RUTA_COLECCION, colections)
+            guardarArchivo(RUTA_MOVIES, movies)
+            pressEnter()
+            break
+    if not encontrado:
+        print('No se encontro nada, registralo.')
+        pressEnter()
+
+
 def editElement():
     while True:
         print(editElementDesign)
@@ -106,10 +156,9 @@ def editElement():
             case 2:
                 editAuthor()
             case 3:
-                #editGenesis
-                pass
+                editGenesis()
             case 4:
-                #editValor
+                #editValor()
                 pass
             case 5:
                 #editCategory()
