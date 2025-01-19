520360d176fe704428861fc4b4f8eb22a287aba3
Jayyy-J <jota71663@gmail.com>
2025-01-19 11:13:25 -0500

    Se agrego la funcion viewBooksCategory()

--- module/viewElementCategory.py
+++ module/viewElementCategory.py
@@ -1,30 +1,81 @@
 from function.function import *
 from design.menssage import *
+from tabulate import tabulate
 
 
 def viewBooksCategory():
+    books = abrirArchivo(RUTA_BOOK)
     while True:
         print(viewcategoryesBooksDesign)
         opc = getInt(':) ')
         match opc:
             case 1:
+                encontrado = False
                 categoria = "novela-grafica"
-                return categoria
+                for book in books:
+                    if categoria == book["categoria"]:
+                        encontrado = True
+                        print(tabulate([categoria], headers="keys", tablefmt="grid"))
+                        pressEnter()
+                        break
+                if not encontrado:
+                    print('Todavia no hay elementos')
+
             case 2:
                 categoria = "comic"
-                return categoria
+                encontrado = False
+                for book in books:
+                    if categoria == book["categoria"]:
+                        encontrado = True
+                        print(tabulate([categoria], headers="keys", tablefmt="grid"))
+                        pressEnter()
+                        break
+                if not encontrado:
+                    print('Todavia no hay elementos')
             case 3:
                 categoria = "humor-grafico"
-                return categoria
+                encontrado = False
+                for book in books:
+                    if categoria == book["categoria"]:
+                        encontrado = True
+                        print(tabulate([categoria], headers="keys", tablefmt="grid"))
+                        pressEnter()
+                        break
+                if not encontrado:
+                    print('Todavia no hay elementos')
             case 4:
                 categoria = "cuento-ilustrado"
-                return categoria
+                encontrado = False
+                for book in books:
+                    if categoria == book["categoria"]:
+                        encontrado = True
+                        print(tabulate([categoria], headers="keys", tablefmt="grid"))
+                        pressEnter()
+                        break
+                if not encontrado:
+                    print('Todavia no hay elementos')
             case 5:
                 categoria = "parodia-literaria"
-                return categoria
+                encontrado = False
+                for book in books:
+                    if categoria == book["categoria"]:
+                        encontrado = True
+                        print(tabulate([categoria], headers="keys", tablefmt="grid"))
+                        pressEnter()
+                        break
+                if not encontrado:
+                    print('Todavia no hay elementos')
             case 6:
                 categoria = "ficcion-satira"
-                return categoria
+                encontrado = False
+                for book in books:
+                    if categoria == book["categoria"]:
+                        encontrado = True
+                        print(tabulate([categoria], headers="keys", tablefmt="grid"))
+                        pressEnter()
+                        break
+                if not encontrado:
+                    print('Todavia no hay elementos')
             case 7:
                 pressEnter()
                 break
