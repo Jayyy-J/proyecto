07df581d4f9112a372848e1344c568ff188db946
Jayyy-J <jota71663@gmail.com>
2025-01-19 12:24:20 -0500

    Se agrego la funcion editAuthor() es funcional

--- module/editElement.py
+++ module/editElement.py
@@ -48,6 +48,54 @@ def editTitle():
         print('No se encontro nada, registralo.')
         pressEnter()
 
+
+def editAuthor():
+    books = abrirArchivo(RUTA_BOOK)
+    musics = abrirArchivo(RUTA_MUSIC)
+    movies = abrirArchivo(RUTA_MOVIES)
+    colections = abrirArchivo(RUTA_COLECCION)
+    autor = input('Escriba el Autor/Artista/Director del elemento que desea cambiar :').capitalize()
+    encontrado = False
+    for book in books:
+        if autor == book["autor"]:
+            encontrado = True
+            newAuthor = input('Escriba el Autor/Artista/Director nuevo :').capitalize()
+            book["autor"] = newAuthor
+            for colection in colections["libros"]:
+                if colection["autor"] == autor:
+                    colection["autor"] = newAuthor
+            guardarArchivo(RUTA_COLECCION, colections)
+            guardarArchivo(RUTA_BOOK, books)
+            pressEnter()
+            break
+    for music in musics:
+        if autor == music["artista"]:
+            encontrado = True
+            newArtist = input('Escriba el Autor/Artista/Director nuevo :').capitalize()
+            music["artista"] = newArtist
+            for colection in colections["musica"]:
+                if colection["artista"] == autor:
+                    colection["artista"] = newArtist
+            guardarArchivo(RUTA_COLECCION, colections)
+            guardarArchivo(RUTA_MUSIC, musics)
+            pressEnter()
+            break
+    for movie in movies:
+        if autor == movie["director"]:
+            encontrado = True
+            newDirector = input('Escriba el Autor/Artista/Director nuevo :').capitalize()
+            music["director"] = newDirector
+            for colection in colections["peliculas"]:
+                if colection["director"] == autor:
+                    colection["director"] = newDirector
+            guardarArchivo(RUTA_COLECCION, colections)
+            guardarArchivo(RUTA_MOVIES, movies)
+            pressEnter()
+            break
+    if not encontrado:
+        print('No se encontro nada, registralo.')
+        pressEnter()
+
 def editElement():
     while True:
         print(editElementDesign)
@@ -56,8 +104,7 @@ def editElement():
             case 1:
                 editTitle()
             case 2:
-                #editAuthor()
-                pass
+                editAuthor()
             case 3:
                 #editGenesis
                 pass
