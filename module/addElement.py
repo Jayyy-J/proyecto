3b44a4e93c157214637899d50b1d57b60d1fdc5c
Jayyy-J <jota71663@gmail.com>
2025-01-19 10:28:20 -0500

    categorias

--- module/addElement.py
+++ module/addElement.py
@@ -56,24 +56,18 @@ def categoryMovies():
         opc = getInt(':) ')
         match opc:
             case 1:
-                categoria = "novela-grafica"
+                categoria = "animacion"
                 return categoria
             case 2:
-                categoria = "comic"
+                categoria = "super-heroes"
                 return categoria
             case 3:
-                categoria = "humor-grafico"
+                categoria = "exprimental"
                 return categoria
             case 4:
-                categoria = "cuento-ilustrado"
+                categoria = "musical-animado"
                 return categoria
             case 5:
-                categoria = "parodia-literaria"
-                return categoria
-            case 6:
-                categoria = "ficcion-satira"
-                return categoria
-            case 7:
                 pressEnter()
                 break
             case _:
@@ -85,16 +79,51 @@ def addMovie(movie, colection):
     genero = input('Escriba el genero de la pelicula :').capitalize()
     valor = getInt('Ingrese la puntuacion de la pelicula (1-5) :')
     valoracion = str(valor)
+    categoria = categoryMovies()
     
     moviesDic = {
         "titulo": titulo,
         "director": autor,
         "genero": genero,
-        "valoracion": valoracion
+        "valoracion": valoracion,
+        "categoria": categoria
     }
     pressEnter()
     movie.append(moviesDic)
     colection["peliculas"].append(moviesDic)
+
+
+def categoryMusic():
+    while True:
+        print(viewcategoryesBooksDesign)
+        opc = getInt(':) ')
+        match opc:
+            case 1:
+                categoria = "videojuegos"
+                return categoria
+            case 2:
+                categoria = "exprimental"
+                return categoria
+            case 3:
+                categoria = "infantil"
+                return categoria
+            case 4:
+                categoria = "pop"
+                return categoria
+            case 5:
+                categoria = "rap"
+                return categoria
+            case 6:
+                categoria = "satira"
+                return categoria
+            case 7:
+                categoria = "funk"
+                return categoria
+            case 8:
+                pressEnter()
+                break
+            case _:
+                print('Ingresa una opcion valida!!!')
 
 def addMusic(musics, colection):
     titulo = input('Escriba el titulo de la cancion :').capitalize()
@@ -102,12 +131,14 @@ def addMusic(musics, colection):
     genero = input('Escriba el genero del disco :').capitalize()
     valor = getInt('Ingrese la puntuacion del disco (1-5) :')
     valoracion = str(valor)
+    categoria = categoryMusic()
     
     musicDic = {
         "titulo": titulo,
         "artista": autor,
         "genero": genero,
-        "valoracion": valoracion
+        "valoracion": valoracion,
+        "categoria": categoria
     }
     pressEnter()
     musics.append(musicDic)
