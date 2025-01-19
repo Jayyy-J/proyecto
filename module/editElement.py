f734cb0f59ae25c06df71b937c1e457eb7f4dd2a
Jayyy-J <jota71663@gmail.com>
2025-01-19 12:11:11 -0500

    Se agrego la funcion editTitle() es funcional

--- module/editElement.py
+++ module/editElement.py
@@ -1,21 +1,72 @@
 from design.menssage import *
 from function.function import *
 
+def editTitle():
+    books = abrirArchivo(RUTA_BOOK)
+    musics = abrirArchivo(RUTA_MUSIC)
+    movies = abrirArchivo(RUTA_MOVIES)
+    colections = abrirArchivo(RUTA_COLECCION)
+    titulo = input('Escriba el titulo del elemento que desea cambiar :').capitalize()
+    encontrado = False
+    for book in books:
+        if titulo == book["titulo"]:
+            encontrado = True
+            newTitle = input('Escriba el titulo nuevo :').capitalize()
+            book["titulo"] = newTitle
+            for colection in colections["libros"]:
+                if colection["titulo"] == titulo:
+                    colection["titulo"] = newTitle
+            guardarArchivo(RUTA_COLECCION, colections)
+            guardarArchivo(RUTA_BOOK, books)
+            pressEnter()
+            break
+    for music in musics:
+        if titulo == music["titulo"]:
+            encontrado = True
+            newTitle = input('Escriba el titulo nuevo :').capitalize()
+            music["titulo"] = newTitle
+            for colection in colections["musica"]:
+                if colection["titulo"] == titulo:
+                    colection["titulo"] = newTitle
+            guardarArchivo(RUTA_COLECCION, colections)
+            guardarArchivo(RUTA_MUSIC, musics)
+            pressEnter()
+            break
+    for movie in movies:
+        if titulo == movie["titulo"]:
+            encontrado = True
+            newTitle = input('Escriba el titulo nuevo :').capitalize()
+            movie["titulo"] = newTitle
+            for colection in colections["peliculas"]:
+                if colection["titulo"] == titulo:
+                    colection["titulo"] = newTitle
+            guardarArchivo(RUTA_COLECCION, colections)
+            guardarArchivo(RUTA_MOVIES, movies)
+            pressEnter()
+            break
+    if not encontrado:
+        print('No se encontro nada, registralo.')
+        pressEnter()
+
 def editElement():
     while True:
         print(editElementDesign)
         opc = getInt(':) ')
         match opc:
             case 1:
-                #editTitle()
+                editTitle()
             case 2:
                 #editAuthor()
+                pass
             case 3:
                 #editGenesis
+                pass
             case 4:
                 #editValor
+                pass
             case 5:
                 #editCategory()
+                pass
             case 6:
                 pressEnter()
                 break
